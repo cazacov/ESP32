d53ee4471fd8129d823ee1a85a558dca6367d783
Victor Cazacov <cazacov.git@gmail.com>
2024-05-11 01:10:15 +0200

    Simple web server

--- WebApi-Arduino/src/main.cpp
+++ WebApi-Arduino/src/main.cpp
@@ -1,23 +1,45 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ArduinoJson.h>
 
-#define LED 23
 
+// Stores ssid and password 
+#include "secrets.h"
+
+AsyncWebServer server(80);
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(LED, OUTPUT);
+  WiFi.setHostname("ESP32_1");
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected.");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", "Welcome to the ESP32 Async REST API");
+  });
+
+  server.onNotFound(notFound);
+
+  server.begin();
 }
 
-int counter = 0;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  counter++;
-  digitalWrite(LED, HIGH);
-  Serial.println("High");
-  delay(2000);
-  digitalWrite(LED, LOW);
-  Serial.println("Low");
-  delay(2000);
+  // No need to call handleClient in the loop for the AsyncWebServer
 }
