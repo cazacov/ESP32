c0b4d2ac9a44e0cfa6ab7da897b2802fe305fe0c
Victor Cazacov <cazacov.git@gmail.com>
2024-05-11 01:36:05 +0200

    Simple JSON post

--- WebApi-Arduino/src/main.cpp
+++ WebApi-Arduino/src/main.cpp
@@ -34,6 +34,29 @@ void setup() {
     request->send(200, "text/plain", "Welcome to the ESP32 Async REST API");
   });
 
+   server.on("/post-json", HTTP_POST, [](AsyncWebServerRequest *request) {}, NULL, [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) 
+   {
+    // Check if the total size of the expected data is greater than zero
+    if (total > 0) {
+      DynamicJsonDocument doc(1024);
+      DeserializationError error = deserializeJson(doc, data);
+      if (error) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+        request->send(400, "text/plain", "Bad Request: JSON parsing failed");
+        return;
+      }
+
+      int intValue = doc["intValue"]; // Extract integer value
+      const char* stringValue = doc["stringValue"]; // Extract string value
+
+      String message = "Received int: " + String(intValue) + ", string: " + stringValue;
+      request->send(200, "text/plain", message);
+    } else {
+      request->send(400, "text/plain", "Bad Request: No JSON found");
+    }
+  });
+
   server.onNotFound(notFound);
 
   server.begin();
